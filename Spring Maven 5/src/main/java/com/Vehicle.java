2b645420fbe8549b3ba0ef15f4f6d0e12b62ab22
SatishSingh0313 <satishsangam.205@gmail.com>
2024-03-15 00:07:45 +0530

    Spring Maven (With bean.xml)

---
+++ Spring Maven 5/src/main/java/com/Vehicle.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Vehicle {
+	
+	private	String	Color;
+
+	public String getColor() {
+		return Color;
+	}
+
+	public void setColor(String Color) {
+		this.Color = Color;
+	}
+	
+}
