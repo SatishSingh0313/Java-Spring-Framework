537758af629cc265e4a37dcb822067e52e3b35c3
SatishSingh0313 <satishsangam.205@gmail.com>
2024-03-16 00:05:44 +0530

    Spring Maven Autowiring (By Name)

---
+++ Spring Maven Autowiring (ByName)/src/main/java/com/sp16/ServiceClass.java
@@ -0,0 +1,24 @@
+package com.sp16;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ServiceClass {
+  public static void main(String[] args) {
+	  
+   ApplicationContext context = new ClassPathXmlApplicationContext("AutowireByName.xml");
+   
+   Certificate c = context.getBean("certi", Certificate.class);
+   
+   System.out.println("Course Name : "+c.getcName());
+   System.out.println("Course Duration: "+c.getcDuration()+" Months");
+   
+   System.out.println("=================================");
+   
+   Student s = context.getBean("stud1",Student.class); 
+   
+   System.out.println("Course Name: "+s.getCerti().getcName());
+   System.out.println("Course Duration: "+s.getCerti().getcDuration()+" Months");
+
+    }
+}
---
+++ Spring Maven Autowiring (ByName)/src/main/java/com/sp16/Student.java
@@ -0,0 +1,15 @@
+package com.sp16;
+
+public class Student {
+	
+	private Certificate certi;
+
+	public Certificate getCerti() {
+		return certi;
+	}
+
+	public void setCerti(Certificate certi) {
+		this.certi = certi;
+	}
+	
+}
